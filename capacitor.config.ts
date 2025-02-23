25c36eac77742ec7057c9d536e6c34345d72ac48
Dimitri <hostd1@bfh.ch>
2025-02-23 18:49:57 +0100

    renamed app identifier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.moreit.hey_coach',
+  appId: 'org.moreit.heycoach',
   appName: 'Hey Coach!',
   webDir: 'dist'
 };
